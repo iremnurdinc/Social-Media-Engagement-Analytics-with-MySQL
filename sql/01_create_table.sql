a4fcccc364e57a7bd77172e082e847ee2ac22435
iremnurdinc <iremirem704@gmail.com>
2025-12-22 18:41:10 +0300

    Update 01_create_table.sql

--- sql/01_create_table.sql
+++ sql/01_create_table.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE social_media_analysis;
+USE social_media_analysis;
+
 CREATE TABLE social_media_posts (
     post_id VARCHAR(50) PRIMARY KEY,
     post_date DATE,
@@ -11,3 +14,20 @@ CREATE TABLE social_media_posts (
     comments INT,
     engagement_level VARCHAR(20)
 );
+
+
+DESC social_media_posts;
+
+--Result:
+--Field            	Type    	   Null	    Key	    Default	    Extra
+--post_id	        varchar(50)	   NO	    PRI	    NULL	
+--post_date	        date	       YES		        NULL	
+--platform	        varchar(50)	   YES		        NULL	
+--hashtag	        varchar(100)   YES		        NULL	
+--content_type	    varchar(50)	   YES		        NULL	
+--region	        varchar(50)	   YES		        NULL	
+--views	            int	YES		   NULL	
+--likes	            int	YES		   NULL	
+--shares	        int	YES		   NULL	
+--comments	        int	YES		   NULL	
+--engagement_level	varchar(20)	   YES		        NULL	
