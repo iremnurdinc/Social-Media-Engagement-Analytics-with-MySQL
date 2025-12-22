d794df5a8e427661e19c423d4808d7a3048f4b4b
iremnurdinc <iremirem704@gmail.com>
2025-12-22 18:26:51 +0300

    Update 03_window_functions.sql

--- sql/sql/sql/03_window_functions.sql
+++ sql/sql/sql/03_window_functions.sql
@@ -8,3 +8,18 @@ FROM (
     FROM social_media_posts
 ) t
 WHERE rank_in_platform <= 3;
+
+--Result:
+--platform	post_id	views	rank_in_platform
+--Instagram	Post_1842	4997766	1
+--Instagram	Post_2508	4992601	2
+--Instagram	Post_2588	4990567	3
+--TikTok	Post_3604	4997284	1
+--TikTok	Post_2106	4979485	2
+--TikTok	Post_3733	4975716	3
+--Twitter	Post_651	4997429	1
+--Twitter	Post_2578	4996111	2
+--Twitter	Post_3784	4988828	3
+--YouTube	Post_676	4999430	1
+--YouTube	Post_2601	4999077	2
+--YouTube	Post_1882	4995973	3
